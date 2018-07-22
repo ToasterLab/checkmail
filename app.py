83595846e5f1a13a6fd1c308089afd5cf663f7dd
Huey <leejinhuey@gmail.com>
2018-07-22 15:39:43 +0800

    added convenience /syntax /dns /smtp methods

--- app.py
+++ app.py
@@ -9,13 +9,37 @@ CORS(app)
 
 @app.route('/')
 def readme():
-    return 'GET /[string: email_address]'
+    return ("<p>GET /[string: email_address]</p><p>GET /overall[string: email_address]</p><p>GET /syntax[string: email_address]</p><p>GET /dns[string: email_address]</p><p>GET /smtp[string: email_address]</p>")
 
 
 @app.route('/<string:address>')
 def check_email(address):
     email = Email(address)
     return jsonify(vars(email))
+
+
+@app.route('/overall/<string:address>')
+def check_email_overall(address):
+    email = Email(address)
+    return str(email.valid['overall']).upper()
+
+
+@app.route('/syntax/<string:address>')
+def check_email_syntax(address):
+    email = Email(address)
+    return str(email.valid['syntax']['result']).upper()
+
+
+@app.route('/dns/<string:address>')
+def check_email_dns(address):
+    email = Email(address)
+    return str(email.valid['dns']['result']).upper()
+
+
+@app.route('/smtp/<string:address>')
+def check_email_smtp(address):
+    email = Email(address)
+    return str(email.valid['smtp']['result']).upper()
 
 
 if(__name__ == '__main__'):
